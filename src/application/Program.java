4c48af93b3f217258270d94783e534ff0ebe279f
Levi Dantas <levibdantas@gmail.com>
2024-09-11 01:35:29 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.time.LocalDate;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,9 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new  Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
